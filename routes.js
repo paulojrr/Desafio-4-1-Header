3eab9e4cead42d104a4bd2740c488713f6546999
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2020-06-29 14:56:30 -0300

    Header

---
+++ routes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const routes = express.Router()
+
+routes.get('/', (req, res) => {
+    res.redirect('/instructors')
+})
+
+routes.get('/instructors', (req, res) => {
+    res.render('instructors/index')
+})
+
+routes.get('/members', (req, res) => {
+    res.send('members')
+})
+
+module.exports = routes
